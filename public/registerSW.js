928dfd6b6e7ebdc0084333dfc57b8119326dcbfe
Kauansantos2git <kauansantos.senai22@gmail.com>
2026-09-23 10:56:17 -0300

    fourth commit

---
+++ public/registerSW.js
@@ -0,0 +1,14 @@
+export function registerServiceWorker() {
+    if ("serviceWorker" in navigator && import.meta.env.PROD) {
+        window.addEventListener("load", () => {
+            navigator.serviceWorker
+                .register("/sw.js")
+                .then((registro) => {
+                    console.log("Service worker registrado: ", registro.scope);
+                })
+                .catch((erro) => {
+                    console.error("Falha ao registrar o service worker: ", erro);
+                })
+        });
+    }
+}
